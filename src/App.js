d863267191d9d8162b211ad2f1039e0720ad1e65
atlonis <veronika.belozechko@gmail.com>
2019-12-28 15:16:15 +0300

    adding styles

--- src/App.js
+++ src/App.js
@@ -46,24 +46,34 @@ class App extends Component {
         country: undefined,
         pressure: undefined,
         sunset: undefined,
-        error: "Введите название города "
+        error: "Введите название города  "
       });
     }
   };
 
   render() {
     return (
-      <div>
-        <Info />
-        <Form weatherMethod={this.gettingWeather} />
-        <Weather
-          temp={this.state.temp}
-          city={this.state.city}
-          country={this.state.country}
-          pressure={this.state.pressure}
-          sunset={this.state.sunset}
-          error={this.state.error}
-        />
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-5 info">
+                <Info />
+              </div>
+              <div className="col-sm-7 form">
+                <Form weatherMethod={this.gettingWeather} />
+                <Weather
+                  temp={this.state.temp}
+                  city={this.state.city}
+                  country={this.state.country}
+                  pressure={this.state.pressure}
+                  sunset={this.state.sunset}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
